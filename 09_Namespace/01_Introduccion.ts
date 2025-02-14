ff8217fccd525a08326fb04f6da2d6d9d0c58228
diego ortega <diego-chido2011@hotmail.com>
2025-02-13 20:12:28 -0600

    NameSpace Parte 1

---
+++ 09_Namespace/01_Introduccion.ts
@@ -0,0 +1,17 @@
+// Nosotros hemos estado creando las funciones anonimas autoinvocadas y esto podria considerarse como un namespace
+// el problema esta en que no podemos acceder a los elementos que contiene dentro
+// Los namespace nis sirve de agrupador para que podamos utilizarlo en cualquier otro lado
+namespace Validations {
+    // Para poder usar estas funciones fuera tenemos que exportarlas
+    // Exportamos solo lo que queremos que se pueda acceder desde afuera del namespace
+    export const validateText = ( text:string ): boolean => {
+        return ( text.length > 3 ) ? true : false;
+    }
+
+    export const validateDate = ( myDate: Date ): boolean => {
+        return ( isNaN( myDate.valueOf() ) ) ? false : true;
+    }
+}
+
+// Uso del NameSpace
+console.log( Validations.validateText('Joe') );
